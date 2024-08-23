ab0220c2eae4321061b534d38cebab3bf01371b3
Nguyen Dinh Trung <73766127+dinhtrung0902@users.noreply.github.com>
2024-08-23 14:31:14 +0700

    Update login.php

--- chucnang/login/login.php
+++ chucnang/login/login.php
@@ -1,27 +1,57 @@
 <?php 
-	if (isset($_SESSION['email'])){
+    if (isset($_SESSION['email'])){
 ?>
 <div id="login" class="col-md-4 col-sm-12 col-xs-12 text-right">
     <div id="login-main">
-        <ul>
-        	<li id="user">Xin chào <?php echo $_SESSION['email'];?>
-                <div>
-                    <ul id="user-main">
-                        <li><a href="./quantri/chucnang/dangxuat/dangxuat.php">Đăng xuất</a></li>
-                    </ul>      
-                </div>
-            </li>
-        </ul>
+        <span class="welcome-text">Xin chào, <?php echo htmlspecialchars($_SESSION['email']); ?></span>
+        <button class="btn btn-logout" onclick="location.href='./quantri/chucnang/dangxuat/dangxuat.php'">Đăng xuất</button>
     </div>
 </div>
 <?php  
-	}else{
+    } else {
 ?>
-<div id="login" class="col-md-4 col-sm-12 col-xs-12 text-right" style="padding-top: 7px; margin-bottom: -4px;">
+<div id="login" class="col-md-4 col-sm-12 col-xs-12 text-right">
     <div id="login-main">
-        <p><button><a href="./quantri/index.php">Đăng nhập</a></button> <span> / </span> <button><a href="./chucnang/tao_tk/taotk.php">Đăng ký</a></button></p>
+        <button class="btn btn-login" onclick="location.href='./quantri/index.php'">Đăng nhập</button>
+        <button class="btn btn-register" onclick="location.href='./chucnang/tao_tk/taotk.php'">Đăng ký</button>
     </div>
 </div>
 <?php  
-	}
+    }
 ?>
+
+<style>
+    #login-main {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+    }
+    .welcome-text {
+        margin-right: 10px;
+        font-weight: bold;
+    }
+    .btn {
+        padding: 8px 16px;
+        margin-left: 10px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        font-weight: bold;
+        transition: background-color 0.3s;
+    }
+    .btn-login {
+        background-color: #4CAF50;
+        color: white;
+    }
+    .btn-register {
+        background-color: #008CBA;
+        color: white;
+    }
+    .btn-logout {
+        background-color: #f44336;
+        color: white;
+    }
+    .btn:hover {
+        opacity: 0.8;
+    }
+</style>
